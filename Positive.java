e6cceb2dc3b22f0bfd709f9ee025b64047426a38
dineshssv91 <dineshssv91@gmail.com>
2016-02-24 10:55:28 +0530

    Added files via upload

---
+++ Positive.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+
+class Positive{
+
+   public static void main(String[] args) { 
+
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Enter a number: ");
+    int number = input.nextInt();
+
+    if( number == 0)
+    { System.out.println("Number is equal to zero"); }
+    else if (number > 0)
+    { System.out.println("Number is positive"); }
+    else 
+    { System.out.println("Number is negative"); }
+
+
+  }
+ }
