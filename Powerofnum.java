75e83c62cb57dfae001ce0d780d1c8e20f1b73c9
dineshssv91 <dineshssv91@gmail.com>
2016-04-13 10:55:20 +0530

    Update Powerofnum.java

--- Powerofnum.java
+++ Powerofnum.java
@@ -5,9 +5,11 @@ class Powerofnum
 {
 public static void main(String[] args)
 {
-int i=2;
-int j=3;
-double k=Math.pow(i,j);
-System.out.println(k);
+Scanner in=new Scanner(System.in);
+System.out.println("Enter the i values");
+int i=in.nextInt();
+System.out.println("Enter the j values");
+int j=in.nextInt();
+System.out.println(Math.pow(i,j));
 }
 }
