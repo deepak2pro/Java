5f589ac4e11234ff37649139b838c6bbb68804fe
dineshssv91 <dineshssv91@gmail.com>
2016-04-13 10:48:29 +0530

    Added files via upload

---
+++ Powerofnum.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+class Powerofnum
+{
+public static void main(String[] args)
+{
+int i=2;
+int j=3;
+double k=Math.pow(i,j);
+System.out.println(k);
+}
+}
