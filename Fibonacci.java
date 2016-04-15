bc243e95953cda57bff218a1d29354a3c46eecfa
dineshssv91 <dineshssv91@gmail.com>
2016-04-15 07:40:37 +0530

    Added files via upload

---
+++ Fibonacci.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+/*0 1 1 2  3 5 8 13 21*/
+class Fibonacci
+{
+public static void main(String[] args){
+int x=0,y=1,z,c;
+Scanner in=new Scanner(System.in);
+System.out.println("Enter the range values");
+c=in.nextInt();
+for(int i=0;i<c;i++){
+z=x+y;
+System.out.println(z);
+x=y;
+y=z;
+}
+}
+}
