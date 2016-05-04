3b31c982d6eed19bad9dd918ff194dc99607c1c1
dineshssv91 <dineshssv91@gmail.com>
2016-05-04 09:02:32 +0530

    Added files via upload

---
+++ Pressq.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+class Pressq
+{
+public static void main(String[] args)
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("Enter the letter");
+String s=sc.nextLine();
+if(s.equals("Q"))
+{
+System.exit(0);
+}
+}
+}
